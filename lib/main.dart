6e9915b8d7dc8ebee124e39527803256f8bb3fab
ALEXANDER_STOG\Stogs <alexanderstogdon@gmail.com>
2023-03-25 18:28:38 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'weather_api.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Happy Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Happy Weather'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _weather = 'unknown';
+  String _location = 'unknown';
+
+  void _getWeatherData() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      double latitude = position.latitude;
+      double longitude = position.longitude;
+      Map<String, dynamic> responseData =
+      await WeatherApi.getWeatherData(latitude, longitude);
+      setState(() {
+        _weather =
+            responseData['weather'][0]['description'].toString().toUpperCase();
+        _location = responseData['name'].toString();
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getWeatherData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_weather',
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Location: $_location',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/weather_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WeatherApi {
+  static const String _apiKey = '2bd91d362af1ba70a21972534d20ecfb';
+
+  static Future<Map<String, dynamic>> getWeatherData(
+      double latitude, double longitude) async {
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&units=metric&appid=$_apiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      throw Exception('Failed to get weather data');
+    }
+  }
+}
